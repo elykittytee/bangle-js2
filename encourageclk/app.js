3b1b42dc3befe103fcd426582e16b894a1a407a9
eleanor <44651387+elykittytee@users.noreply.github.com>
2023-03-28 10:04:32 -0500

    Update app.js

--- encourageclk/app.js
+++ encourageclk/app.js
@@ -1,20 +1,19 @@
+require("FontHaxorNarrow7x17").add(Graphics);
+require("FontDylex7x13").add(Graphics);
+
 const storage = require('Storage');
 const locale = require("locale");
 const dateutil = require("date_utils");
-require("FontHaxorNarrow7x17").add(Graphics);
-require("FontDylex7x13").add(Graphics);
 const currentFont=g.getFont();
 
 const width = 175; 
 const height = 175;
-
 const offset = 25;
 
 var d = new Date();
 var nowDate = d.getDate(); //today's date
-var encourage = var encourage = ["you\'re doing great!","pas de deux it!","you\'re amazing~","you got dis","keep going","you\'re one\nin a melon!","we\'re rooting\nfor you!","believe in\nyourself","dance like\nno one\'s\nwatching"];
+var encourage = ["you\'re doing great!","pas de deux it!","you\'re amazing~","you got dis","keep going","you\'re one\nin a melon!","we\'re rooting\nfor you!","believe in\nyourself","dance like\nno one\'s\nwatching"];
 var encouragementtest = "you\'re one\nin a melon!";
-
 
 //TAP ALL THE THINGS
 Bangle.on('touch', (n, e) => {
@@ -22,8 +21,7 @@ Bangle.on('touch', (n, e) => {
   if (e.x > offset && e.y < height) { //bar
     clearText();
     g.setColor(0,0,0);
-    //g.setFont("Dylex7x13",2).setFontAlign(0,0).drawString(getEncour(), width/2, height/2);
-    g.setFont("Dylex7x13",2).setFontAlign(0,0).drawString(encouragement, width/2, height/2);
+    g.setFont("Dylex7x13",2).setFontAlign(0,0).drawString(getEncour(), width/2, height/2);
     setInterval(loader,3000);
   }
 });
